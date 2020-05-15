d4723ad10439b8c9c8e8233113c6415946c25dd9
Abhishek Gupta <blp.abhishek@yahoo.com>
2020-05-15 16:20:36 +0530

    Rename the environment varibale name.

--- prepareReport.js
+++ prepareReport.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const report = require('../report.json');
 
-const META_EVENT = process.env.META_EVENT;
+const HEAD_COMMIT = process.env.HEAD_COMMIT;
 const TEST_REPO_NAME = process.env.TEST_REPO_NAME;
 
-const commit = JSON.parse(META_EVENT);
+const commit = JSON.parse(HEAD_COMMIT);
 const tests = report.tests;
 const stats = report.stats;
 
@@ -15,10 +15,10 @@ const getUserName = function (author) {
 const reportWithMeta = {
   username: getUserName(commit.author),
   timestamp: commit.timestamp,
-  tests,
   stats,
-  commit,
-  test_repo_name: TEST_REPO_NAME
+  test_repo_name: TEST_REPO_NAME,
+  tests,
+  commit
 };
 
 fs.writeFileSync('report-with-meta.json', JSON.stringify(reportWithMeta));
